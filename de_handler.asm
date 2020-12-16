b017308167c5a75ebd7525de1673c876ab584b6c
Ofir-Manor <ofir.manor@campus.technion.ac.il>
2020-12-16 08:10:17 +0000

    first attempt, not checkec, not quite sure about inline assmebly

--- de_handler.asm
+++ de_handler.asm
@@ -7,3 +7,27 @@
 .align 4, 0x90
 my_de_handler:
   #STUDENT NEED TO FILL
+	pushq %rbp #save old rbp
+	movq %rsp, %rbp # move rbp to top
+	
+	pushq %rdi
+	pushq %r8
+	
+	movq %rax, %rdi #sends the divisor to teh first argument
+	
+	call *what_to_do #get handler response
+	
+	xor %r8, %r8  
+	cmp %rax, %r8  #cmp handler response to 0
+	jne end
+	
+	call *old_de_handler #get old handler response
+	
+	end:
+		popq %r8
+		popq %rdi
+	
+		leave
+		ret
+	
+	
--- de_utils.c
+++ de_utils.c
@@ -2,24 +2,63 @@
 
 void my_store_idt(struct desc_ptr *idtr) {
 //# <STUDENT FILL>
+	asm volatile("movq %%idtr, %%rdi;"
+	"leave;"
+	"ret;"
+	:
+	:
+	:
+	);
 	// TODO: if we omit inline assembly:  store_idt(&tmpidtr);
 // </STUDENT FILL>
 }
 
 void my_load_idt(struct desc_ptr *idtr) {
 // <STUDENT FILL>
+	asm volatile("movq %%rdi, %%idtr;"
+	"leave;"
+	"ret;"
+	:
+	:
+	:
+	);
 	// if we omit inline assembly: load_idt(addr);
 // <STUDENT FILL>
 }
 
 void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
 // <STUDENT FILL>
+//*gate = rdi addr =rsi
+	asm volatile("pushq %%rax;"
+	"movq %%rsi, %%rax;"
+	"movw %%ax, (%%rdi);"
+	"shrq %16, %%rax;"
+	"movw %%ax, 6(%%rdi);"
+	"shrq %15, %%rax;"
+	"movl %%eax, 8(%%rdi);"
+	"popq %%rax;"
+	
+	"leave;"
+	"ret;"
+	:
+	:
+	:
+	);
+	
 	// TODO: pack_gate(gate, GATE_INTERRUPT, addr, 0, 0, __KERNEL_CS);
 // </STUDENT FILL>
 }
 
 unsigned long my_get_gate_offset(gate_desc *gate) {
 // <STUDENT FILL>
+	asm volatile("movq %%rdi, %%rax;"
+	"sub %%idtr, %%rax;"
+	"leave;"
+	"ret;"
+	:
+	:
+	:
+	);
 	// TODO: return gate_offset(gate);
 // </STUDENT FILL>
 }
